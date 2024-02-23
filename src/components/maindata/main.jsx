8194332d8d8eaf61bbc9116b57041e235ba6778a
VinothS0107 <138591668+VinothS0107@users.noreply.github.com>
2024-02-23 12:12:49 +0530

    main.jsx

--- src/components/maindata/main.jsx
+++ src/components/maindata/main.jsx
@@ -3,20 +3,22 @@ import { Context } from "../../context/useContext"
 import icon from "../../assets/sunny.jpg"
 export function MainContent(){
     const apiData=useContext(Context)[2]
+    const cityDetails=useContext(Context)[0]
      const temperature=apiData.hourly.temperature_2m
      const time=apiData.hourly.time
     
-
    let combine=[]
    for(let i=0;i<temperature.length;i++){
         combine.push([temperature[i], new Date(time[i]).getHours()])
  }
 console.log(combine)
     return(
-        <div className="grid grid-cols-4 gap-2 sm:w-[80%] mt-[30px] justify-items-center content-center">
+        <>
+        <h1 className="text-[18px] mt-5 font-bold">City: {cityDetails.city}</h1>
+        <div className="grid grid-cols-4 gap-2 sm:w-[80%] mt-[30px] justify-items-center content-center">    
             {combine.map((eachItem)=>(
                 <>
-                    <div className="border rounded-md w-[80px] h-[80px] flex flex-col items-center" >
+                    <div className="border rounded-md w-[80px] h-[80px] md:w-[100px]  flex flex-col items-center" >
                         <p>{eachItem[1]}</p>
                         <img src={icon} className="w-[30px] h-[30px]" />
                         <p>{eachItem[0]}<sup>o</sup>C</p>
@@ -25,7 +27,7 @@ console.log(combine)
             </>
                 )
             )}
-
         </div>
+        </>
     )
 }
